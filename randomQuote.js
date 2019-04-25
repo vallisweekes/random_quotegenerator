7194a2e347842a7b828f8ec5216135742f652634
vallisweekes <46267296+vallisweekes@users.noreply.github.com>
2019-04-25 22:27:05 +0100

    Update randomQuote.js

--- randomQuote.js
+++ randomQuote.js
@@ -1,91 +1,112 @@
-//Quote Generator 
-// 'use strict'
-welcome()
 
-function welcome() {
-    console.log('Your Daily Quote Generator');
-    quoteType()
-}
+/********************Random Quote Generator Project *****************
+1. The quotes are generated randomly one by one with each JavaScript call.
+2.Each quote is a combination of at least 3 sentence fragments (it's up to 
+you to create these sentence fragments: beginning, middle, and end)
+3.The quotes should be relatively coherent (no exclamation points in the middle of a sentence, for example)
+4.Select the number of quotes generated (between 1 and 5)
+5.Choose between 2 types of quote generators (accordingly, you'll need two different sets of sentences)
+6.Once the quotes are generated, have the program generate new quotes or quit the program
+  */  
+    
+    quoteGenerator()
 
-function quoteType (type) {
 
-    var type = prompt('For Animal Quotes Enter: 1\nFor Flowers Quotes Enter: 2\n\nEnter Choice');
+    //ANIMAL QUOTE GENERATOR 
+    function randomAnimalQuotes(number) {
+        
+        // Take User Input 
+        var number = prompt('Enter How Many Quotes Woul You Like To See')
+        
+        if (number >= '1' && number <= '5') {
+            console.log('Your Daily Quote Generator');
+            console.log('Here Are Your Animal Quotes As Requested');
+            
+            //Quote Fragments 
 
-    if (type === '1' || type === '2') {
-        switch (type) {
-            case '1':
-                console.log(randomAnimalQuotes());
-                return;
-
-            case '2':
-                console.log(randomFlowersQuotes());
-                return;
+            const fragBeginning = ['These are very beautiful ', 'I like looking at ', ' Today We saw some new ', 'There a nice '];
+            
+            const fragMiddle = ['animal pictures', 'cattle horns', 'sheep wool', 'fast cars', 'mobile phones'];
+            
+            const fragEnd = [' i would love to buy one', ' they are very nice', ' they are so breath taking', ' in the shop'];
+            
+            
+            var quotes = [];
+            
+            for (var i = 0; i < number; i++) {
+                
+                const fragBeginningRandom = [Math.floor(Math.random() * fragBeginning.length)];
+                const fragMiddelRandom = [Math.floor(Math.random() * fragMiddle.length)];
+                const fragEndRandom = [Math.floor(Math.random() * fragBeginning.length)];
+                const fullQuote = [fragBeginning[fragBeginningRandom] + fragMiddle[fragMiddelRandom] + fragEnd[fragEndRandom]];
+                quotes.push(fullQuote);
+                
+            }
+            return quotes;
+            
+        } else {
+            alert('Please enter and number between 1 and 5');
         }
-    } 
-}
-
-// function userQuit () {
-//     console.log(alert('Have a good day'));
-// }
-function randomAnimalQuotes(number) {
-    var number = prompt('Enter How Many Quotes You Would Like')
-
-    if (number >= '1' && number <= '5') {
-        console.log('Here are your Animal quotes as requested');
-
-        const fragBeginning = ['These are very beautiful ', 'I like looking at ', ' Today We saw some new ', 'There a nice '];
-
-        const fragMiddle = ['animal pictures', 'cattle horns', 'sheep wool', 'fast cars', 'mobile phones'];
-
-        const fragEnd = [' i would love to buy one', ' they are very nice', ' they are so breath taking', ' in the shop'];
-
-        //Random generated quotes
-
-        var quotes = [];
-
-        for (var i = 0; i < number; i++) {
-
-            const fragBeginningRandom = [Math.floor(Math.random() * fragBeginning.length)];
-            const fragMiddelRandom = [Math.floor(Math.random() * fragMiddle.length)];
-            const fragEndRandom = [Math.floor(Math.random() * fragBeginning.length)];
-            const fullQuote = [fragBeginning[fragBeginningRandom] + fragMiddle[fragMiddelRandom] + fragEnd[fragEndRandom]];
-            quotes.push(fullQuote);
-
-        }
-        return quotes;
-
-    } else {
-        alert('Please enter and number between 1 and 5');
-        randomAnimalQuotes()
     }
-}
+    
+    //FLOWERS QUOTE GENERATOR
+    
+    function randomFlowersQuotes(number) {
+         // Take User Input 
+        var number = prompt('Enter How Many Quotes You Would Like');
+    
+        if (number >= '1' && number <= '5') {
+            console.log('Your Daily Quote Generator');
+            console.log('Here Are Your Flowers Quotes As Requested');
+            
+            //Quote Fragments 
 
-function randomFlowersQuotes(number) {
-
-    var number = prompt('Enter How Many Quotes You Would Like');
-
-    if (number >= '1' && number <= '5') {
-
-        console.log('Here are your Flowers quotes as requested')
-
-        const fragBeginning = ['These are very beautiful ', 'I like looking at ', ' Today We saw some new ', 'There a nice '];
-        const fragMiddle = ['flower pots', 'hibiscuss trees', 'red roses', 'lily flowers', 'rare flowers'];
-        const fragEnd = [' i would love to buy one', ' they are very nice', ' they are so breathtaking', ' in the shop'];
-
-
-        var quotes = [];
-
-        for (var i = 0; i < number; i++) {
-
-            const fragBeginningRandom = [Math.floor(Math.random() * fragBeginning.length)];
-            const fragMiddelRandom = [Math.floor(Math.random() * fragMiddle.length)];
-            const fragEndRandom = [Math.floor(Math.random() * fragBeginning.length)];
-            const fullQuote = [fragBeginning[fragBeginningRandom] + fragMiddle[fragMiddelRandom] + fragEnd[fragEndRandom]];
-            quotes.push(fullQuote);
+            const fragBeginning = ['These are very beautiful ', 'I like looking at ', ' Today We saw some new ', 'There a nice '];
+            const fragMiddle = ['flower pots', 'hibiscuss trees', 'red roses', 'lily flowers', 'rare flowers'];
+            const fragEnd = [' i would love to buy one', ' they are very nice', ' they are so breathtaking', ' in the shop'];
+            
+            
+            var quotes = [];
+            
+            for (var i = 0; i < number; i++) {
+                
+                const fragBeginningRandom = [Math.floor(Math.random() * fragBeginning.length)];
+                const fragMiddelRandom = [Math.floor(Math.random() * fragMiddle.length)];
+                const fragEndRandom = [Math.floor(Math.random() * fragBeginning.length)];
+                const fullQuote = [fragBeginning[fragBeginningRandom] + fragMiddle[fragMiddelRandom] + fragEnd[fragEndRandom]];
+                quotes.push(fullQuote);
+            }
+            return quotes;
+        } else {
+            alert('Please enter and number between 1 and 5');
         }
-        return quotes;
-    } else {
-        alert('Please enter and number between 1 and 5');
-        randomFlowersQuotes()
     }
-}
+    
+    //Initiating the programme
+
+    function quoteGenerator(type) {
+    
+        var type = prompt('For Animal Quotes Enter: 1\nFor Flowers Quotes Enter: 2\nTo Quit Enter: 0 \n\nEnter Choice');
+        
+    while (type !== '0') {
+                switch (type) {
+                    case '1':
+                    console.log(randomAnimalQuotes());
+                    break;
+                    
+                    case '2':
+                    console.log(randomFlowersQuotes());
+                    break;
+
+                    default: alert('Invalid choice try again');
+              
+            }
+            var type = prompt('For Animal Quotes Enter: 1\nFor Flowers Quotes Enter: 2\nTo Quit Enter: 0 \n\nEnter Choice');
+    
+        }
+        if(type === '0') {
+         alert('Have A Great day, Good Bye.')
+        }
+        
+    }
+    ///END OF PROGRAMME ////
